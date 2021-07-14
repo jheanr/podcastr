46f4dd8cedd227a6123579194802e040e40e1362
Jhean Ramos <jheanr@gmail.com>
2021-07-14 16:55:37 -0300

    fix db.json exceed limit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: ' https://my-json-server.typicode.com/jheanr/podcastr'
+  baseURL: 'https://my-json-server.typicode.com/jheanr/podcastr'
 })
